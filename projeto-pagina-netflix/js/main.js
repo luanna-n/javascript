6603630b478d17577c1a4fd9bfb0fa37515f53dc
luanna-n <nayara.luanna@hotmail.com>
2021-05-13 22:40:07 -0300

    personaliza o projeto inicial, codeflix agora

--- projeto-pagina-netflix/js/main.js
+++ projeto-pagina-netflix/js/main.js
@@ -4,13 +4,13 @@ $('.owl-carousel').owlCarousel({
     nav: false, //são os <> de navegação => true
     responsive: {
         0: {
-            items: 1 //proximo de 0px => 1 elemento
+            items: 3 //proximo de 0px => 1 elemento
         },
         600: { //quando a tela tem +600px => 3 elementos
-            items: 3
+            items: 5
         },
         1000: { //quando a tela tem +1000 px => 5 elementos
-            items: 5
+            items: 8
         }
     }
 })
