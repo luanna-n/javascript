17087526802a7bdf06a95649b31cbb4263415d66
luanna-n <nayara.luanna@hotmail.com>
2021-05-16 17:34:11 -0300

    define direcoes e impede que a cobrinha suma

--- projeto-jogo-cobrinha/script.js
+++ projeto-jogo-cobrinha/script.js
@@ -21,7 +21,24 @@ function criarCobrinha() {
     }
 }
 
+//evento de escuta
+
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left"; //adicionar o && impede que ela faça o movimento oposto por cima de si mesma
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "right") direction = "down";
+}
+
+//criar o jogo
 function iniciarJogo() {
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
 
